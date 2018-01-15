a9a0c0d116a46cef79954a605891eef807849972
backtake <stasiusoltysik@gmail.com>
2018-01-15 17:05:54 +0100

    Implement constructors

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,6 +1,24 @@
 package com.codecool.dynamicArrayDojo;
 
-// put your code here!
 public class DynamicIntArray {
+    
+    int[] array;
+    
+    public DynamicIntArray(int size){
+        if (size < 0) throw new IllegalArgumentException("Cannot accept negative size");
+        this.array = new int[size];
+    }
+    
+    public DynamicIntArray(){
+        this.array = new int[10];
+    }
 
+    public void add(int i) {
+    }
+
+    public void remove(int i) {
+    }
+
+    public void insert(int i, int i1) {
+    }
 }
